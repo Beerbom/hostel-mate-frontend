5360b7fafe7b38aff9e16a803e7e8e40d98da5b0
mohdnishal <nishalutd@gmail.com>
2024-03-03 10:34:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Form from './pages/Form'
+import axios from 'axios';
 
+axios.defaults.baseURL='http://localhost:5000';
+axios.defaults.withCredentials=true
 function App() {
   return (
     <div className="App">
